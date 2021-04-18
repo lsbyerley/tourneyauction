179e97e765c67d078d141e059c4a765d309ccb27
lsbyerley <lsbyerley@gmail.com>
2021-04-18 17:28:33 -0400

    refactor

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,74 @@
-import '../styles/globals.css';
+import { useState, useEffect, Fragment } from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0';
+import ProgressBar from '@badrap/bar-of-progress';
+import Head from 'next/head';
+import Router from 'next/router';
+import { Title } from '@/components/Title';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
-  );
+const progress = new ProgressBar({
+  size: 2,
+  color: '#22D3EE',
+  className: 'bar-of-progress',
+  delay: 100,
+});
+
+// this fixes safari jumping to the bottom of the page
+// when closing the search modal using the `esc` key
+if (typeof window !== 'undefined') {
+  progress.start();
+  progress.finish();
 }
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', () => {
+  progress.finish();
+  window.scrollTo(0, 0);
+});
+Router.events.on('routeChangeError', progress.finish);
+
+const MyApp = ({ Component, pageProps }) => {
+  let [navIsOpen, setNavIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (!navIsOpen) return;
+    function handleRouteChange() {
+      setNavIsOpen(false);
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [navIsOpen]);
+
+  const Layout = Component.layoutProps?.Layout || Fragment;
+  const layoutProps = Component.layoutProps?.Layout
+    ? { layoutProps: Component.layoutProps, navIsOpen, setNavIsOpen }
+    : {};
+
+  const meta = Component.layoutProps?.meta || {};
+  const description =
+    meta.metaDescription || meta.description || 'Golf Tourney Auction';
+
+  return (
+    <>
+      <Title suffix='Golf Tourney Auction'>
+        {meta.metaTitle || meta.title}
+      </Title>
+      <Head>
+        <meta
+          key='og:description'
+          property='og:description'
+          content={description}
+        />
+      </Head>
+      <UserProvider>
+        <Layout {...layoutProps}>
+          <Component {...pageProps} />
+        </Layout>
+      </UserProvider>
+    </>
+  );
+};
 
 export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 import { GraphQLClient } from 'graphql-request';
-import Layout from '@/components/Layout';
-import { useUser } from '@auth0/nextjs-auth0';
+import BasicLayout from '@/layouts/BasicLayout';
+import { UserAddIcon, MailIcon, PhoneIcon } from '@heroicons/react/solid';
 
 const graphcms = new GraphQLClient(process.env.GRAPHQL_URL_ENDPOINT);
 
@@ -25,66 +25,117 @@ export async function getStaticProps() {
     `
   );
 
+  const { sports } = await graphcms.request(
+    `
+    query Sports() {
+      sports {
+        name
+        league
+      }
+    }
+    `
+  );
+
+  const { auctions } = await graphcms.request(
+    `
+    query Auctions() {
+      auctions {
+        id
+        createdAt
+        name
+        sport
+        startDate
+        endDate
+      }
+    }
+    `
+  );
+
   return {
     props: {
       destinations,
+      sports,
+      auctions,
     },
   };
 }
 
-export default function Home({ destinations }) {
-  const { user, error, isLoading } = useUser();
-
-  console.log('LOG: from UserProvider in index.js', user, error, isLoading);
-
+export default function Home({ destinations, sports, auctions }) {
   return (
     <div>
       <Head>
-        <title>GraphCMS Golf Starter</title>
+        <title>Home Golf Auction</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Layout>
-        <main>
-          <h1>Destinations</h1>
-
-          <ul>
-            <li>
-              <Link href='/'>
-                <a>Home</a>
-              </Link>
-            </li>
-            <li>
-              <Link href='/about'>
-                <a>About</a>
-              </Link>
-            </li>
-            <li>
-              <Link href='/login'>
-                <a>Login</a>
-              </Link>
-            </li>
-          </ul>
-
-          {destinations.map((d) => {
-            // console.log('LOG: d', d);
+      <main>
+        <ul className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'>
+          {auctions.map((auction) => {
+            console.log('LOG: auction', auction);
             return (
-              <div key={d.id}>
-                {d.image && (
-                  <div className='w-40'>
-                    <Image
-                      src={d.image.url}
-                      width={d.image.width}
-                      height={d.image.height}
-                    />
+              <li
+                key={auction.id}
+                className='col-span-1 bg-white divide-y divide-gray-200 rounded-lg shadow'
+              >
+                <div className='flex items-center justify-between w-full p-6 space-x-6'>
+                  <div className='flex-1 truncate'>
+                    <div className='flex items-center space-x-3'>
+                      <h3 className='text-sm font-medium text-gray-900 truncate'>
+                        {auction.name}
+                      </h3>
+                      <span className='flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full'>
+                        {auction.sport}
+                      </span>
+                    </div>
+                    <p className='mt-1 text-sm text-gray-500 truncate'>
+                      {auction.startDate}
+                    </p>
                   </div>
-                )}
-                <p>{d.name}</p>
-                <p>{d.description}</p>
-              </div>
+                  <img
+                    className='flex-shrink-0 w-10 h-10 bg-gray-300 rounded-full'
+                    src={'insert user picture here'}
+                    alt=''
+                  />
+                </div>
+                <div>
+                  <div className='flex -mt-px divide-x divide-gray-200'>
+                    <div className='flex flex-1 w-0'>
+                      <a
+                        href='#'
+                        className='relative inline-flex items-center justify-center flex-1 w-0 py-4 -mr-px text-sm font-medium text-gray-700 border border-transparent rounded-bl-lg hover:text-gray-500'
+                      >
+                        <MailIcon
+                          className='w-5 h-5 text-gray-400'
+                          aria-hidden='true'
+                        />
+                        <span className='ml-3'>Join</span>
+                      </a>
+                    </div>
+                    <div className='flex flex-1 w-0 -ml-px'>
+                      <a
+                        href='#'
+                        className='relative inline-flex items-center justify-center flex-1 w-0 py-4 text-sm font-medium text-gray-700 border border-transparent rounded-br-lg hover:text-gray-500'
+                      >
+                        <PhoneIcon
+                          className='w-5 h-5 text-gray-400'
+                          aria-hidden='true'
+                        />
+                        <span className='ml-3'>Entry Code</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </li>
             );
           })}
-        </main>
-      </Layout>
+        </ul>
+      </main>
     </div>
   );
 }
+
+Home.layoutProps = {
+  meta: {
+    title: 'Home',
+  },
+  Layout: BasicLayout,
+};
